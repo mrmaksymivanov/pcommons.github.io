2e58cd3d9e26a8d73ce2e76816277a70392ff644
Paul Commons <paulcommons@gmail.com>
2015-08-20 23:42:38 +0200

    adding partner prequals

--- widget/widget.js
+++ widget/widget.js
@@ -32,31 +32,136 @@
 	}
 
 
-/* Altering these defaults may break retrotax-aci.com/plugin-demo */
-if (typeof window._retrotax_options == "undefined"){
-	var _retrotax_options = {
-            iframe_base_path: 'https://pcommons.github.io/widget/iframe',  //http://plugin.retrotax-aci.com/widget/iframe',
-            debug: false,
-            username:'DemoAPI.New.Employee',
-            apikey:'111BC0B55FEF6737944B37B1CA2DBED3',
-            button_class:"btn btn-huge btn-info",
-            button_text:"Open RetroTax Screening Plugin",
-            framework:"material-design",
-            plugin_type: 'demo',  
-            prepopulate_basic_info_by_id: {
-                firstname:'first_name',
-                lastname:'last_name',
-                middleinitial:'middle_init',
-                city:'',
-                state:'',
-                stateid:'',
-                zip:'',
-                address:'',
-                address2:'',
-                countyid:'',
-                dob:''
-            }
-        };
+/**************************************
+
+
+Partnering organizations and prequal orgs custom config
+
+
+***************************************/
+var pluginChapman = document.getElementById('chapman_plugin');
+if (typeof(pluginChapman) != 'undefined' && pluginChapman != null){
+	/* Altering these defaults may break chapman prequal */
+	if (typeof window._retrotax_options == "undefined"){
+		var _retrotax_options = {
+	            iframe_base_path: 'https://pcommons.github.io/widget/iframe',  //http://plugin.retrotax-aci.com/widget/iframe',
+	            debug: false,
+	            username:'DemoAPI.New.Employee',
+	            apikey:'111BC0B55FEF6737944B37B1CA2DBED3',
+	            button_class:"btn btn-huge btn-info",
+	            button_text:"Open RetroTax & Chapman Tax Credit Screening",
+	            framework:"material-design",
+	            plugin_type: 'prequal'
+	            logo:'',
+				prequal = {
+			                email_to:'',
+			                email_cc:'',
+			                partner_name:'',
+			                partner_organization:'',
+			                partner_website:'',
+			                closing_text:'',
+			                intro_text:'',
+			                logo_url:'',
+			                logo_width:'', 
+			                logo_height:'',                
+			                retrotax_contact:'natalie.commons@retrotax-aci.com',
+			                ask_ssn:false
+			    }
+	        };
+	}
+}
+
+var pluginPCG = document.getElementById('pcg_plugin');
+if (typeof(pluginPCG) != 'undefined' && pluginPCG != null){
+	/* Altering these defaults may break chapman prequal */
+	if (typeof window._retrotax_options == "undefined"){
+		var _retrotax_options = {
+	            iframe_base_path: 'https://pcommons.github.io/widget/iframe',  //http://plugin.retrotax-aci.com/widget/iframe',
+	            debug: false,
+	            username:'DemoAPI.New.Employee',
+	            apikey:'111BC0B55FEF6737944B37B1CA2DBED3',
+	            button_class:"btn btn-huge btn-info",
+	            button_text:"Open Public Consulting Group Tax Credit Screening",
+	            framework:"material-design",
+	            plugin_type: 'prequal',
+	            logo:'',
+				prequal = {
+			                email_to:'',
+			                email_cc:'',
+			                partner_name:'',
+			                partner_organization:'',
+			                partner_website:'',
+			                closing_text:'',
+			                intro_text:'',
+			                logo_url:'',
+			                logo_width:'', 
+			                logo_height:'',                
+			                retrotax_contact:'john.hess@retrotax-aci.com',
+			                ask_ssn:false
+			    }
+	        };
+	}
+}
+
+var pluginMW = document.getElementById('mw_plugin');
+if (typeof(pluginMW) != 'undefined' && pluginMW != null){
+	/* Altering these defaults may break chapman prequal */
+	if (typeof window._retrotax_options == "undefined"){
+		var _retrotax_options = {
+	            iframe_base_path: 'https://pcommons.github.io/widget/iframe',  //http://plugin.retrotax-aci.com/widget/iframe',
+	            debug: false,
+	            username:'DemoAPI.New.Employee',
+	            apikey:'111BC0B55FEF6737944B37B1CA2DBED3',
+	            button_class:"btn btn-huge btn-info",
+	            button_text:"Open Manufacturing Works Tax Credit Screening",
+	            framework:"material-design",
+	            plugin_type: 'prequal',
+	            logo:'',
+				prequal = {
+			                email_to:'',
+			                email_cc:'',
+			                partner_name:'',
+			                partner_organization:'',
+			                partner_website:'',
+			                closing_text:'',
+			                intro_text:'',
+			                logo_url:'',
+			                logo_width:'', 
+			                logo_height:'',                
+			                retrotax_contact:'john.hess@retrotax-aci.com',
+			                ask_ssn:false
+			    }           
+	        };
+	}
+}
+
+var pluginRetroTax = document.getElementById('retrotax_demo');
+if (typeof(pluginRetroTax) != 'undefined' && pluginRetroTax != null){
+	if (typeof window._retrotax_options == "undefined"){
+		var _retrotax_options = {
+	            iframe_base_path: 'https://pcommons.github.io/widget/iframe',  //http://plugin.retrotax-aci.com/widget/iframe',
+	            debug: false,
+	            username:'DemoAPI.New.Employee',
+	            apikey:'111BC0B55FEF6737944B37B1CA2DBED3',
+	            button_class:"btn btn-huge btn-info",
+	            button_text:"Open RetroTax Screening Plugin",
+	            framework:"material-design",
+	            plugin_type: 'demo',  
+	            prepopulate_basic_info_by_id: {
+	                firstname:'first_name',
+	                lastname:'last_name',
+	                middleinitial:'middle_init',
+	                city:'',
+	                state:'',
+	                stateid:'',
+	                zip:'',
+	                address:'',
+	                address2:'',
+	                countyid:'',
+	                dob:''
+	            }
+	        };
+	}
 }
 
 
